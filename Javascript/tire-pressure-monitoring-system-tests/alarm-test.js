70a32e40fa78d15cd20d59a1188b55b57e76665d
Sungwon Ma <sungwon.ma.16@gmail.com>
2020-05-13 19:47:58 -0400

    Start writing a test for Alarm

--- Javascript/tire-pressure-monitoring-system-tests/alarm-test.js
+++ Javascript/tire-pressure-monitoring-system-tests/alarm-test.js
@@ -2,11 +2,11 @@ describe('Tyre Pressure Monitoring System', function () {
 
 	describe('Alarm', function () {
 
-		it('Do something', function () {
+		it('gets turned on when pressure is too low', function () {
 
 			var target = new Alarm();
 			target.check();
-			
+
 		});
 
 	});
