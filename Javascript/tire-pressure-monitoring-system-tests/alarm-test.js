e6ec856bb00051406dd99c858d056132b392e87f
Sungwon Ma <sungwon.ma.16@gmail.com>
2020-05-13 20:06:06 -0400

    Add test for not turning on the alarm when the pressure is within range

--- Javascript/tire-pressure-monitoring-system-tests/alarm-test.js
+++ Javascript/tire-pressure-monitoring-system-tests/alarm-test.js
@@ -3,22 +3,30 @@ describe('Tyre Pressure Monitoring System', function () {
 	describe('Alarm', function () {
 
 		it('gets turned on when pressure is too low', function () {
-      var sensor = {popNextPressurePsiValue: () => 16};
+			var sensor = { popNextPressurePsiValue: () => 16 };
 			var target = new Alarm(sensor);
 			target.check();
 
-      expect(target.alarmOn()).toEqual(true);
+			expect(target.alarmOn()).toEqual(true);
 		});
 
-    it('gets turned on when pressure is too high', function() {
-      var sensor = { popNextPressurePsiValue: () => 22 };
-      var target = new Alarm(sensor);
+		it('gets turned on when pressure is too high', function () {
+			var sensor = { popNextPressurePsiValue: () => 22 };
+			var target = new Alarm(sensor);
 
-      target.check();
+			target.check();
 
-      expect(target.alarmOn()).toEqual(true);
-    });
+			expect(target.alarmOn()).toEqual(true);
+		});
 
+		it('remains off when pressure is within range', function () {
+			var sensor = { popNextPressurePsiValue: () => 20 };
+			var target = new Alarm(sensor);
+
+			target.check();
+
+			expect(target.alarmOn()).toEqual(false);
+		});
 	});
 
 });
